1aaa86405a8ceb67b95ffa4912a2115f0ccb8633
deepikaP556 <panchoduladeepikamanidurga8121@gmail.com>
2026-09-21 21:59:23 +0530

    BucketSort

--- BucketSort.java
+++ BucketSort.java
@@ -1,44 +1,59 @@
-import java.io.*;
+
 import java.util.*;
 
-public class BucketSort {
-
+public class Solution {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
-
         double[] arr = new double[n];
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++)
             arr[i] = sc.nextDouble();
-        }
 
-        ArrayList<Double>[] bucket = new ArrayList[10];
-
-        for (int i = 0; i < 10; i++) {
-            bucket[i] = new ArrayList<>();
-        }
+        
+        boolean small = true;
 
         for (int i = 0; i < n; i++) {
-            int idx = (int)(arr[i] * 10);
-            bucket[idx].add(arr[i]);
-        }
-
-        for (int i = 0; i < 10; i++) {
-            Collections.sort(bucket[i]);
-        }
-
-        int index = 0;
-
-        for (int i = 0; i < 10; i++) {
-            for (double num : bucket[i]) {
-                arr[index++] = num;
+            if (arr[i] >= 1) {
+                small = false;
+                break;
             }
         }
 
-        for (int i = 0; i < n; i++) {
-            System.out.printf("%.2f ", arr[i]);
+        if (small) {
+            ArrayList<Double>[] bucket = new ArrayList[n];
+
+            for (int i = 0; i < n; i++)
+                bucket[i] = new ArrayList<>();
+
+            for (int i = 0; i < n; i++) {
+                int index = (int)(arr[i] * n);
+
+                if (index == n)
+                    index = n - 1;
+
+                bucket[index].add(arr[i]);
+            }
+
+            for (int i = 0; i < n; i++)
+                Collections.sort(bucket[i]);
+
+            for (int i = 0; i < n; i++)
+                for (double x : bucket[i])
+                    System.out.printf("%.2f ", x);
+        }
+        else {
+            
+            Arrays.sort(arr);
+
+            for (int i = 0; i < n; i++) {
+                if (arr[i] == (int)arr[i])
+                    System.out.print((int)arr[i] + " ");
+                else
+                    System.out.printf("%.2f ", arr[i]);
+            }
         }
     }
 }
